fd53aa4b03b79b4bfb2e9f6fcb909c44cdbd2092
Jairo Daniel Bautista <bautisj@colpatria.com>
2018-07-16 13:56:42 -0500

    Enhacing Swagger Documentation with Custom Anotations

--- src/main/java/com/dany/rest/webservices/restfulwebservices/SwaggerConfig.java
+++ src/main/java/com/dany/rest/webservices/restfulwebservices/SwaggerConfig.java
@@ -2,11 +2,16 @@ package com.dany.rest.webservices.restfulwebservices;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by bautisj on 7/5/2018.
@@ -15,8 +20,29 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    public static final Contact DEFAULT_CONTACT = new Contact(
+            "Daniel Bautista",
+            "http://www.dany.com",
+            "jdanielbc193@gmail.com"
+    );
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES =
+            new HashSet<String> (Arrays.asList("application/json", "aplication/xml"));
+
+    private ApiInfo DEFAULT_API_INFO = new ApiInfo(
+            "Awesome API title",
+            "Awesome API Documentation",
+            "1.0",
+            "urn:tos",DEFAULT_CONTACT,
+            "Apache 2.0",
+            "http://www.apache.org/licenses/LICENSE-2.0",
+            new ArrayList()
+        );
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                    .apiInfo(DEFAULT_API_INFO)
+                    .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                    .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 }
--- src/main/java/com/dany/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/dany/rest/webservices/restfulwebservices/user/User.java
@@ -1,6 +1,8 @@
 package com.dany.rest.webservices.restfulwebservices.user;
 
 import com.dany.rest.webservices.restfulwebservices.post.Post;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
@@ -10,13 +12,16 @@ import java.util.Date;
 /**
  * Created by bautisj on 6/29/2018.
  */
+@ApiModel(description = "All details to the user.")
 public class User {
 
     private Integer id;
     @Size(min = 2, message = "Name shoud have at least 2 characters")
+    @ApiModelProperty(notes="Name shoud have at least 2 characters")
     private String name;
 
     @Past(message = "The Date must be in the past")
+    @ApiModelProperty(notes="Birth date should be in the past")
     private Date birthDate;
     private ArrayList<Post> userPosts;
 
